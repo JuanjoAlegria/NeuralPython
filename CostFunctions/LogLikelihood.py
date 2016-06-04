8f718a5363110d465e1d274c3defbd0d2249b27e
Juanjo Alegría <jotaj.8a@gmail.com>
2016-06-03 23:43:10 -0400

    Bugfix en funciones de costo

--- CostFunctions/LogLikelihood.py
+++ CostFunctions/LogLikelihood.py
@@ -2,8 +2,9 @@ import numpy as np
 
 class LogLikelihood:
     def function(self, actualOutput, desiredOutput):
-        return -desiredOutput * np.log(actualOutput)
-
+        if isinstance(desiredOutput, np.ndarray):
+            desiredOutput = np.argmax(desiredOutput)
+        return -np.log(actualOutput[desiredOutput])
     def derivative(self,actualOutput, desiredOutput):
         a_y = actualOutput[desiredOutput]
         return -1.0/a_y
--- CostFunctions/Quadratic.py
+++ CostFunctions/Quadratic.py
@@ -2,7 +2,11 @@ import numpy as np
 
 class Quadratic:
     def function(self, actualOutput, desiredOutput):
-        return 0.5 * (desiredOutput - actualOutput)**2
+        # if isinstance(desiredOutput, int):
+        #     d = desiredOutput
+        #     desiredOutput = np.zeros(actualOutput.shape)
+        #     desiredOutput[d] = 1
+        return 0.5 * np.linalg.norm(desiredOutput - actualOutput)**2
 
     def derivative(self,actualOutput, desiredOutput):
         return actualOutput - desiredOutput
